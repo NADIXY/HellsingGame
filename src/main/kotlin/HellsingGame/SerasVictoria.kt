90c5af3a37a814a77b3ce20c81d6818494355cfa
NadiaXY <gbnadia025@gmail.com>
2023-11-08 22:38:46 +0100

    Letzten Spiel änderungen

--- src/main/kotlin/HellsingGame/SerasVictoria.kt
+++ src/main/kotlin/HellsingGame/SerasVictoria.kt
@@ -7,46 +7,63 @@ class SerasVictoria(name: String, hp: Int, waffe: String, element: String, schad
     Helden(name, hp, waffe, element, schaden) {
 
     override fun heilZauber() {
+        println("Aktion: heilZauber ")
         hp += 40
-        println("${this.name} hat '40HP +' dazu bekommen, sich mit dem Schutz Zauber geschützt." +
-                    "\n${this.name}'s HP beträgt jetzt ${this.hp}HP."
+        println(
+            "${this.name} hat '40HP +' dazu bekommen, sich mit dem Schutz Zauber geschützt." + "\n${this.name}'s HP beträgt jetzt ${this.hp}HP."
         )
     }
-
 
     override fun aktionen(gegner: Gegner) {
         println("Held ${this.name} führt eine Attacke aus.")
         println(
             """
-            "A" -> xxxx
+            "A" -> herzExplosion
             "B" -> waffeAttacke
-            "C" -> attacke3
+            "C" -> weekDaysFluch
             "D" -> Heil Zauber
             
         """.trimIndent()
         )
         val eingabe = readln().uppercase()
         when (eingabe) {
-            "A" -> xxxx(gegner)
+            "A" -> herzExplosion(GEGNER1)
             "B" -> waffeAttacke(gegner)
-            "C" -> attacke3()
-            "D" -> attacke3()
+            "C" -> weekDaysFluch(gegner)
+            "D" -> heilZauber()
             else -> println("Eingabe Falsch!")
         }
     }
 
-    fun xxxx(gegner: Gegner) {}
+    fun herzExplosion(gegner1: List<Gegner>) {
+        println("Aktion: herzExplosion ")
+        for (gegner in GEGNER1) {
+            val schaden = 2 * 70
+            gegner.hp -= schaden
+            println(
+                "${this.name} hat Held ${gegner.name} 1 Teil des Herzens Explodiert und die HP reduziert," + "\nHeld ${gegner.name} hat jetzt nur noch ${gegner.hp}HP übrig!"
+            )
+        }
 
+    }
 
-override fun waffeAttacke(gegner: Gegner) {
-    val schaden = Random.nextInt(25..35)
-    gegner.hp -= schaden
-    println("$name greift ${gegner.name} mit der $waffe an," +
-            " fügt Schaden zu und reduziert die" +
-            " HP der Gegner.\n ${gegner.name} hat jetzt nur noch ${gegner.hp}HP übrig!")
+    override fun waffeAttacke(gegner: Gegner) {
+        println("Aktion: waffeAttacke ")
+        val schaden = Random.nextInt(25..35)
+        gegner.hp -= schaden
+        println(
+            "$name greift ${gegner.name} mit der $waffe an,fügt Schaden zu und reduziert die HP der Gegner." + "\n ${gegner.name} hat jetzt nur noch ${gegner.hp}HP übrig!"
+        )
+    }
 
-}
-fun attacke3() {}
+    fun weekDaysFluch(gegner: Gegner) {
+        println("Aktion: weekDaysFluch ")
+        val schaden = Random.nextInt(schaden.first, schaden.last * 7)
+        gegner.hp -= schaden
+        println(
+            "${this.name} verflucht mit der Fluch 'weekDaysFluch', einmal ${gegner.name} verflucht," + "\n für 1 Woche, jeden Tag kriegt 1 Kollaps " + "\n${gegner.name} hat jetzt nur noch ${gegner.hp}HP übrig!"
+        )
+    }
 
     override fun beutelAktion(beutel: Beutel) {
         println("Wählen Sie:\n* -> heiltrank\n# -> vitamine")
