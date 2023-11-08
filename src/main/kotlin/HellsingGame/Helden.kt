77c672d64f4222faa15528d70474a47b5fa5775c
NadiaXY <gbnadia025@gmail.com>
2023-11-08 22:01:43 +0100

    Letzten Spiel änderungen

--- src/main/kotlin/HellsingGame/Helden.kt
+++ src/main/kotlin/HellsingGame/Helden.kt
@@ -1,9 +1,10 @@
 package HellsingGame
-
 import kotlin.random.Random
 import kotlin.random.nextInt
 
-open class Helden(val name: String, var hp: Int, val waffe: String, val element: String, var schaden: IntRange) {
+open class Helden(
+    val name: String, var hp: Int, val waffe: String, val element: String, var schaden: IntRange
+) {
 
     open fun aktionAusfuehren() {
         println(
@@ -22,43 +23,21 @@ open class Helden(val name: String, var hp: Int, val waffe: String, val element:
         }
     }
 
-    open fun beutelAktion(beutel: Beutel) {
-        println("...")
-
-    }
-
     open fun heilZauber() {
         println("Held heilt sich mit dem Heil Zauber.")
     }
 
     open fun aktionen(gegner: Gegner) {
-        println("1 Held führt eine Attacke aus.")
-        println(
-            """
-            "A" -> x
-            "B" -> waffeAttacke
-            "C" -> attacke3
-            "D" -> Heil Zauber
-            
-        """.trimIndent()
-        )
-        val eingabe = readln().uppercase()
-        when (eingabe) {
-            "A" -> x(gegner)
-            "B" -> waffeAttacke(gegner)
-            "C" -> attacke3(gegner)
-            "D" -> heilZauber()
-            else -> println("Eingabe Falsch!")
-        }
+        println("1 Aktion wird ausgesucht und ausgeführt")
     }
 
-    open fun x(gegner: Gegner) {}
+    open fun waffeAttacke(gegner: Gegner) {
+        println("Ein Gegner wird mit 1 Waffe attackiert!")
+    }
 
-        open fun waffeAttacke(gegner: Gegner) {
-            println("Ein Gegner wird mit 1 Waffe attackiert!")
-        }
-
-        fun attacke3(gegner: Gegner) {}
+    open fun beutelAktion(beutel: Beutel) {
+        println("Held wird sich heilen...")
+    }
 
 
 }
