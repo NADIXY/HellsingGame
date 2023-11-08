c4ac77cf09d3b651b974f5919baba2317ca96940
NadiaXY <gbnadia025@gmail.com>
2023-11-09 00:00:38 +0100

    Letzten Spiel änderungen

--- src/main/kotlin/HellsingGame/Beutel.kt
+++ src/main/kotlin/HellsingGame/Beutel.kt
@@ -2,44 +2,32 @@ package HellsingGame
 
 class Beutel {
 
-    var heiltrankGenutzt = false
 
-    var inventar : MutableList<String> = mutableListOf()
+
+    var inventar: MutableList<String> = mutableListOf("Heiltrank", "Vitamine")
 
     fun heiltrank(held: Helden) {
-        inventar.add("Heiltrank")
-        println("Lebenspunkte wird um die hälfte erhöht.")
-        val portionenHeiltrank = held.hp / 2
-        if (inventar.contains("Heiltrank") && !heiltrankGenutzt) {
-            println("${held.name} wurde mit der Heiltrank geheilt,\n" +
-                    " HP erhöht sich um ${held.hp}HP.")
-            inventar.remove("Heiltrank")
-            heiltrankGenutzt = true
-        }else if (!inventar.contains("Heiltrank")) {
-            println("Im Inventar ist keinen Heiltrank mehr verfügbar!")
-        }else{
-            println("In diesem 'Level' habt Ihr mit dem Heiltrank bereits geheilt!")
-        }
+        val heiltrank = "Heiltrank"
+        val heilung = held.hp / 2
+        held.hp += heilung
+        println("${held.name} hat mit dem $heiltrank sich um $heilung geheilt.\n" +
+                " Jetzt beträgt der HP ${held.hp}HP!")
     }
+
 
     fun vitamine(held: Helden) {
-        val hpPlus = 0.1
-        if (inventar.contains("Vitamine") && !heiltrankGenutzt) {
-            held.hp = (1 + hpPlus).toInt()
-            println(" Held ${held.name} hat dauerhaft die HP ${held.hp * 100}% erhöht!\n " +
-                    "HP beträgt jetzt ${held.hp}")
-            inventar.remove("Vitamine")
-            heiltrankGenutzt = true
-        }else if (!inventar.contains("Vitamine")) {
-            println("Im Inventar ist keine Vitamine mehr verfügbar!")
-        }else{
-            println("In diesem 'Level' habt Ihr bereits geheilt!")
-        }
-
+        val aufpusch = "Vitamine"
+        held.schaden = (held.schaden.first * 1.1).toInt()..(held.schaden.last * 1.1).toInt()
+        held.schaden + held.hp
+        println(
+            "${held.name} hat einen Schadensbonus von 10% erhalten mit dem $aufpusch erhalten," + "\n jetzt beträgt der HP ${held.hp}HP"
+        )
     }
-
-
 }
+
+
+
+
 
 
 
